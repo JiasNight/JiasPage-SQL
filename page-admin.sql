d02d24f65e301413dcd4f797a5374e95a19fdded
JiasNight <Jias96@163.com>
2024-01-09 23:53:59 +0800

    菜单

--- page-admin.sql
+++ page-admin.sql
@@ -8,7 +8,7 @@
 */
 CREATE DATABASE IF NOT EXISTS `pageDB`  DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
---DROP TABLE IF EXISTS `SYS_USERS`;
+DROP TABLE IF EXISTS `SYS_USERS`;
 CREATE TABLE IF NOT EXISTS `SYS_USERS`(
 	`user_id` varchar(50) NOT NULL COMMENT '用户id',
   `user_name` varchar(20) NULL DEFAULT NULL COMMENT '用户账号',
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `SYS_USERS`(
 ) ENGINE = InnoDB
 
 
-INSERT INTO `SYS_USERS` VALUES ('aa8e31e6-c155-4694-b654-6f25a19bfde5', 'xiaozhang', '123456', '小张', '123@163.com', '13977943560', 1, '1995-12-21', '昆明', '/images/avatar/xiaozhang.png', '没有的才是最后的', 0, NULL, NULL, NULL);
+INSERT INTO `SYS_USERS` VALUES ('aa8e31e6-c155-4694-b654-6f25a19bfde5', 'xiaozhang', '123456', '小张', '123@163.com', '13977943560', 1, '1995-12-21', '昆明', '/images/avatar/xiaozhang.png', '没有的才是最后的', 0, 'admin', NULL, NULL);
 INSERT INTO `SYS_USERS` VALUES ('b8c51c2f-fdd2-4ab6-838c-6c5e15c5fe6a', 'zhangsan', '123456', '张三', 'zhangsan@163.com', '13977943560', 1, '1995-4-11', '昆明', '/images/avatar/zhangsan.png', '最后的才是最好的', 0, NULL, NULL, NULL);
 INSERT INTO `SYS_USERS` VALUES ('52bb4814-ffca-4d1e-90ff-8eb33b10ef99', 'pinpin', '112233', '萍萍', 'liupin@163.com', '13977943560', 0, '1996-8-11', '四川', '/images/avatar/pinpin.png', '在出现以前我会等', 0, NULL, NULL, NULL);
 INSERT INTO `SYS_USERS` VALUES ('7bb41f4f-d744-45ab-84bb-57ce4b972542', 'libai', '$2a$10$B8HpBwd3BDH7ddae0MDqMu6r9avRyiqN9KciDT0eoTS3keLA/nw5e', '李白', '123456@163.com', '12345678910', 0, '2020-10-08', '上海', NULL, '好好学习', 0, NULL, NULL, NULL);
@@ -40,4 +40,4 @@ INSERT INTO `SYS_USERS` VALUES ('09375fcd-f7bd-46b8-a4da-340e14c76079', 'aabbcc'
 INSERT INTO `SYS_USERS` VALUES ('62226da9-45fc-49a6-b3a2-49d953d53d83', 'xxxxx', 'xxxxx', '', '', '', 0, '', '', '', '', 0, NULL, '2020-08-18 16:21:50', '2020-08-18 16:21:50');
 INSERT INTO `SYS_USERS` VALUES ('d39491b9-51da-4f35-bd8b-0842be056317', 'xiaobai', 'xxxxx', '小白白', '25987365@qq.com', '689534782', 0, '2008-12-23', '四川', '/images/avatar/xiaobai.png', '爱疯了方法', 0, '2020-08-18 16:24:29', '2020-08-18 16:24:29');
 INSERT INTO `SYS_USERS` VALUES ('b6c36892-adef-4e07-9f06-9cc595378388', 'hahh', '332211', '小哈', '25448766@qq.com', '1357846987', 1, '2008-12-23', '成都', '/images/avatar/hh.png', '刚刚去噶啥', 0, '2020-08-18 16:38:16', '2020-08-18 16:38:16');
-INSERT INTO `SYS_USERS` VALUES ('ed72ab50-b85b-45f1-963e-a29b3b359f3b', 'vivi', '$2a$10$QKUsaTxUCfcdDW9jDZxSbeb/IN0SG4SzTygr7tnbNc6isE8JePCLy', '微微', '1689157364', '13977945698', 1, '2004-5-20', '曲靖', '/images/avatar/vivi.png', '天生我材必有用', 0, '2020-08-18 17:23:05', '2020-08-18 17:23:05');
+INSERT INTO `SYS_USERS` VALUES ('ed72ab50-b85b-45f1-963e-a29b3b359f3b', 'vivi', '123456', '微微', 'vv@qq.com', '13977945698', 1, '2004-5-20', '曲靖', '/images/avatar/vivi.png', '天生我材必有用', 0, 'admin', '2020-08-18 17:23:05', '2020-08-18 17:23:05');
