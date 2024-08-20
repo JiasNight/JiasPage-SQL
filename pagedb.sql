9a3a2a8108e0d51a5c902c7f2cdd53abd5b6a508
JiasNight <Jias96@163.com>
2024-08-20 22:34:24 +0800

    修改

--- pagedb.sql
+++ pagedb.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80034
  File Encoding         : 65001
 
- Date: 19/07/2024 18:00:30
+ Date: 20/08/2024 22:33:45
 */
 
 SET NAMES utf8mb4;
@@ -76,15 +76,15 @@ CREATE TABLE `sys_menu`  (
 -- ----------------------------
 INSERT INTO `sys_menu` VALUES ('0525fd7e-06b7-4c03-bae5-4f92af3138d6', '674113dc-0286-4faf-869f-f4ebf51391b7', '登录日志', 'signInLog', 'mdiLogin', 'logManage/signInLog', 1, 0, 1, 1, '/system/logManage/signInLog', '登录日志', 2, 0, 'admin', '2024-04-17 16:46:23', 'admin', '2024-04-17 16:46:23');
 INSERT INTO `sys_menu` VALUES ('0a1969567e1842a18cac009ef0d1f79f', '6fa28cf73c6c4c11bec106553d69c1b9', '菜单管理', 'MenuManage', 'mdiMenu', 'menuManage', 1, 0, 1, 1, '/system/menuManage/index', '菜单管理', 3, 0, 'admin', '2023-10-04 14:27:49', 'admin', '2024-04-17 16:30:00');
-INSERT INTO `sys_menu` VALUES ('3bf0941cbcdc4f95b03e4096997ed18d', '6fa28cf73c6c4c11bec106553d69c1b9', '用户管理', 'UserManage', 'ic:baseline-settings', 'userManage', 1, 0, 1, 1, '/system/userManage/index', '用户管理', 1, 0, 'admin', '2023-10-04 14:27:49', 'admin', '2023-12-30 14:27:58');
-INSERT INTO `sys_menu` VALUES ('674113dc-0286-4faf-869f-f4ebf51391b7', '6fa28cf73c6c4c11bec106553d69c1b9', '日志管理', 'logManage', 'mdi:math-log', 'logManage', 0, 0, 1, 1, 'aa', '日志管理', 6, 0, 'admin', '2024-04-17 16:19:36', 'admin', '2024-04-17 16:40:42');
-INSERT INTO `sys_menu` VALUES ('6fa28cf73c6c4c11bec106553d69c1b9', '0', '系统管理', 'SystemManage', 'ic:baseline-settings', '/system', 0, 0, 1, 1, NULL, '系统管理', 2, 0, 'admin', '2023-10-04 14:27:49', 'admin', '2023-12-30 14:27:58');
+INSERT INTO `sys_menu` VALUES ('3bf0941cbcdc4f95b03e4096997ed18d', '6fa28cf73c6c4c11bec106553d69c1b9', '用户管理', 'UserManage', 'mdiAccountMultiple', 'userManage', 1, 0, 1, 1, '/system/userManage/index', '用户管理', 1, 0, 'admin', '2023-10-04 14:27:49', 'admin', '2023-12-30 14:27:58');
+INSERT INTO `sys_menu` VALUES ('674113dc-0286-4faf-869f-f4ebf51391b7', '6fa28cf73c6c4c11bec106553d69c1b9', '日志管理', 'logManage', 'mdiMathLog', 'logManage', 0, 0, 1, 1, 'aa', '日志管理', 6, 0, 'admin', '2024-04-17 16:19:36', 'admin', '2024-04-17 16:40:42');
+INSERT INTO `sys_menu` VALUES ('6fa28cf73c6c4c11bec106553d69c1b9', '0', '系统管理', 'SystemManage', 'mdiCogs', '/system', 0, 0, 1, 1, NULL, '系统管理', 2, 0, 'admin', '2023-10-04 14:27:49', 'admin', '2023-12-30 14:27:58');
 INSERT INTO `sys_menu` VALUES ('71fa2550-ec9b-4037-8bdb-95f837f7a748', '674113dc-0286-4faf-869f-f4ebf51391b7', '操作日志', 'operLog', 'mdiMathLog', 'logManage/operLog', 1, 0, 1, 1, '/system/logManage/operLog', '操作日志', 1, 0, 'admin', '2024-04-17 16:44:39', 'admin', '2024-04-17 16:44:39');
-INSERT INTO `sys_menu` VALUES ('74c3f4f1-2245-44fa-b9f8-74716af5f1a4', '6fa28cf73c6c4c11bec106553d69c1b9', '字典管理', 'dictManage', 'mdi:dictionary', 'dictManage', 1, 0, 1, 1, 'dictManage/index', '字典管理', 5, 0, 'admin', '2024-04-17 16:21:49', 'admin', '2024-04-17 16:42:34');
+INSERT INTO `sys_menu` VALUES ('74c3f4f1-2245-44fa-b9f8-74716af5f1a4', '6fa28cf73c6c4c11bec106553d69c1b9', '字典管理', 'dictManage', 'mdiDictionary', 'dictManage', 1, 0, 1, 1, 'dictManage/index', '字典管理', 5, 0, 'admin', '2024-04-17 16:21:49', 'admin', '2024-04-17 16:42:34');
 INSERT INTO `sys_menu` VALUES ('ab62e594a25d4c0dab54cda018b850ee', '0', '文章管理', 'ArticleManage', 'ic:outline-article', '/article', 0, 0, 1, 1, '', '文章管理', 1, 0, 'admin', '2023-10-04 14:27:49', 'admin', '2023-12-30 14:27:58');
 INSERT INTO `sys_menu` VALUES ('b547c33203994ae3afa48c4807a25b8f', 'ab62e594a25d4c0dab54cda018b850ee', '发布文章', 'ReleaseArticle', 'ic:outline-article', 'releaseArticle', 1, 0, 1, 1, '/article/index', '发布文章', 1, 0, 'admin', '2023-10-04 14:27:49', 'admin', '2023-12-30 14:27:58');
 INSERT INTO `sys_menu` VALUES ('c42c7755-ef28-4c83-ab61-bca1ceb3c1c0', 'ab62e594a25d4c0dab54cda018b850ee', '测试', '123', NULL, '/teset', 1, 0, 1, 1, '/tese', '13', NULL, 1, 'admin', '2024-07-10 20:13:40', NULL, '2024-07-10 20:13:40');
-INSERT INTO `sys_menu` VALUES ('d9fa698b-ea02-4066-9ff5-3bef72a86a85', '6fa28cf73c6c4c11bec106553d69c1b9', '角色管理', 'roleManage', 'mdi:account-key', 'roleManage', 1, 0, 1, 1, '/system/roleManage/index', '角色管理', 2, 0, 'admin', '2024-04-17 16:20:10', 'admin', '2024-04-17 16:32:22');
+INSERT INTO `sys_menu` VALUES ('d9fa698b-ea02-4066-9ff5-3bef72a86a85', '6fa28cf73c6c4c11bec106553d69c1b9', '角色管理', 'roleManage', 'mdiAccountKey', 'roleManage', 1, 0, 1, 1, '/system/roleManage/index', '角色管理', 2, 0, 'admin', '2024-04-17 16:20:10', 'admin', '2024-04-17 16:32:22');
 INSERT INTO `sys_menu` VALUES ('ec84932c-d7fa-4aab-abc3-a1429522d3ae', '6fa28cf73c6c4c11bec106553d69c1b9', '部门管理', 'deptManage', 'mdiFileTree', 'deptManage', 1, 0, 1, 1, '/system/deptManage/index', '部门管理', 4, 0, 'admin', '2024-04-17 16:15:01', 'admin', '2024-04-17 16:42:10');
 
 -- ----------------------------
@@ -162,9 +162,9 @@ CREATE TABLE `sys_user`  (
   `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '电话号码',
   `gender` tinyint(1) NULL DEFAULT NULL COMMENT '用户性别，0表示女，1表示男',
   `birthday` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '出生日期',
-  `city` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '所在城市',
+  `city_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '所在城市的id',
   `avatar` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '/images/avatar/default.png' COMMENT '用户头像',
-  `marks` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '个性签名',
+  `remarks` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
   `is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除，0表示没有删除，1表示删除',
   `create_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建人',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
@@ -176,15 +176,15 @@ CREATE TABLE `sys_user`  (
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('52bb4814-ffca-4d1e-90ff-8eb33b10ef99', 'pinpin', '$2a$10$7xJ.hVhM3DMOd6XHAdL3EecIM.l5XhcFYUw8JGXDGVdNYGPQ4vomK', '萍萍', 'liupin@163.com', '13977943560', 0, '1996-8-11', '四川', '/images/avatar/pinpin.png', '在出现以前我会等', 0, NULL, '2020-08-18 16:38:16', NULL, '2020-08-18 16:38:16');
-INSERT INTO `sys_user` VALUES ('74eee865-0cbd-438d-91ff-b3b1371d8c28', 'admin', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '超级管理员', 'admin@page.com', '18888888888', 1, '2000-01-01', '云南', '/images/avatar/admin.png', '超级无敌管理员', 0, 'admin', '2023-12-29 17:57:33', NULL, '2023-12-29 17:57:33');
-INSERT INTO `sys_user` VALUES ('7bb41f4f-d744-45ab-84bb-57ce4b972542', 'libai', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '李白', '123456@163.com', '12345678910', 0, '2020-10-08', '上海', NULL, '好好学习', 0, NULL, '2020-08-18 16:38:16', NULL, '2020-08-18 16:38:16');
-INSERT INTO `sys_user` VALUES ('aa8e31e6-c155-4694-b654-6f25a19bfde5', 'xiaozhang', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '小张', '123@163.com', '13977943560', 1, '1995-12-21', '昆明', '/images/avatar/xiaozhang.png', '没有的才是最后的', 0, NULL, '2020-08-18 16:38:16', NULL, '2020-08-18 16:38:16');
-INSERT INTO `sys_user` VALUES ('b6c36892-adef-4e07-9f06-9cc595378388', 'hahh', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '小哈', '25448766@qq.com', '1357846987', 1, '2008-12-23', '成都', '/images/avatar/hh.png', '刚刚去噶啥', 0, 'admin', '2020-08-18 16:38:16', NULL, '2020-08-18 16:38:16');
-INSERT INTO `sys_user` VALUES ('b8c51c2f-fdd2-4ab6-838c-6c5e15c5fe6a', 'zhangsan', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '张三', 'zhangsan@163.com', '13977943560', 1, '1995-4-11', '昆明', '/images/avatar/zhangsan.png', '最后的才是最好的', 0, NULL, '2020-08-18 16:38:16', NULL, '2020-08-18 16:38:16');
-INSERT INTO `sys_user` VALUES ('d39491b9-51da-4f35-bd8b-0842be056317', 'xiaobai', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '小白白', '25987365@qq.com', '689534782', 0, '2008-12-23', '四川', '/images/avatar/xiaobai.png', '爱疯了方法', 0, 'admin', '2020-08-18 16:24:29', NULL, '2020-08-18 16:24:29');
+INSERT INTO `sys_user` VALUES ('52bb4814-ffca-4d1e-90ff-8eb33b10ef99', 'pinpin', '$2a$10$7xJ.hVhM3DMOd6XHAdL3EecIM.l5XhcFYUw8JGXDGVdNYGPQ4vomK', '萍萍', 'liupin@163.com', '13977943560', 0, '1996-8-11', '四川', '/images/avatar/pinpin.png', '在出现以前我会等', 0, 'admin', '2020-08-18 16:38:16', 'admin', '2020-08-18 16:38:16');
+INSERT INTO `sys_user` VALUES ('74eee865-0cbd-438d-91ff-b3b1371d8c28', 'admin', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '超级管理员', 'admin@page.com', '18888888888', 1, '2000-01-01', '云南', '/images/avatar/admin.png', '超级无敌管理员', 0, 'admin', '2023-12-29 17:57:33', 'admin', '2023-12-29 17:57:33');
+INSERT INTO `sys_user` VALUES ('7bb41f4f-d744-45ab-84bb-57ce4b972542', 'libai', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '李白', '123456@163.com', '12345678910', 0, '2020-10-08', '上海', NULL, '好好学习', 0, 'admin', '2020-08-18 16:38:16', 'admin', '2020-08-18 16:38:16');
+INSERT INTO `sys_user` VALUES ('aa8e31e6-c155-4694-b654-6f25a19bfde5', 'xiaozhang', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '小张', '123@163.com', '13977943560', 1, '1995-12-21', '昆明', '/images/avatar/xiaozhang.png', '没有的才是最后的', 0, 'admin', '2020-08-18 16:38:16', 'admin', '2020-08-18 16:38:16');
+INSERT INTO `sys_user` VALUES ('b6c36892-adef-4e07-9f06-9cc595378388', 'hahh', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '小哈', '25448766@qq.com', '1357846987', 1, '2008-12-23', '成都', '/images/avatar/hh.png', '刚刚去噶啥', 0, 'admin', '2020-08-18 16:38:16', 'admin', '2020-08-18 16:38:16');
+INSERT INTO `sys_user` VALUES ('b8c51c2f-fdd2-4ab6-838c-6c5e15c5fe6a', 'zhangsan', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '张三', 'zhangsan@163.com', '13977943560', 1, '1995-4-11', '昆明', '/images/avatar/zhangsan.png', '最后的才是最好的', 0, 'admin', '2020-08-18 16:38:16', 'admin', '2020-08-18 16:38:16');
+INSERT INTO `sys_user` VALUES ('d39491b9-51da-4f35-bd8b-0842be056317', 'xiaobai', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '小白白', '25987365@qq.com', '689534782', 0, '2008-12-23', '四川', '/images/avatar/xiaobai.png', '爱疯了方法', 0, 'admin', '2020-08-18 16:24:29', 'admin', '2020-08-18 16:24:29');
 INSERT INTO `sys_user` VALUES ('d4fe12ba-0d9a-4e94-a2c8-a560e4ce8382', 'root', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '超级管理员', 'admin@page.com', '18888888888', 1, '2000-01-01', '云南', '/images/avatar/admin.png', '超级无敌管理员', 0, 'admin', '2024-05-30 22:31:37', 'admin', '2024-05-30 22:31:37');
-INSERT INTO `sys_user` VALUES ('ed72ab50-b85b-45f1-963e-a29b3b359f3b', 'vivi', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '微微', 'vv@qq.com', '13977945698', 1, '2004-5-20', '曲靖', '/images/avatar/vivi.png', '天生我材必有用', 0, 'admin', '2020-08-18 17:23:05', NULL, '2020-08-18 17:23:05');
+INSERT INTO `sys_user` VALUES ('ed72ab50-b85b-45f1-963e-a29b3b359f3b', 'vivi', '$2a$10$YBv/v.BCfFxDxw4ztNXFC.O9Nz25nMSsMpihLNaaA7gG37HuEv.3m', '微微', 'vv@qq.com', '13977945698', 1, '2004-5-20', '曲靖', '/images/avatar/vivi.png', '天生我材必有用', 0, 'admin', '2020-08-18 17:23:05', 'admin', '2020-08-18 17:23:05');
 
 -- ----------------------------
 -- Table structure for sys_user_role
